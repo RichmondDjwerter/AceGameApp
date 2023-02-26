3823cd350c1acb5ec55a167417a26e47ee7209a6
Djwerter Richmond Tetteh <Astrixrichmond@gmail.com>
2023-02-26 20:24:47 +0000

    SplashScreen Implementation

--- app/src/main/java/com/richmondprojects/acegameapp/MainActivity.kt
+++ app/src/main/java/com/richmondprojects/acegameapp/MainActivity.kt
@@ -3,17 +3,26 @@ package com.richmondprojects.acegameapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.richmondprojects.acegameapp.presentation.home.HomeViewModel
 import com.richmondprojects.acegameapp.ui.theme.AceGameAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply { setKeepOnScreenCondition { viewModel.splashScreen.value } }
         setContent {
             AceGameAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -21,9 +30,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    HomePage()
                 }
             }
         }
     }
+}
+
+@Composable
+fun HomePage() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) { Text(text = "HomePage") }
 }
---
+++ app/src/main/java/com/richmondprojects/acegameapp/presentation/home/HomeViewModel.kt
@@ -0,0 +1,37 @@
+package com.richmondprojects.acegameapp.presentation.home
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.richmondprojects.acegameapp.data.repository.RepositoryImplementation
+import com.richmondprojects.acegameapp.domain.model.Games
+import com.richmondprojects.acegameapp.domain.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repositoryImpl: RepositoryImplementation
+) : ViewModel() {
+
+    private val _splashScreenVisible = mutableStateOf(false)
+    val splashScreen: State<Boolean>
+        get() = _splashScreenVisible
+
+    private val _games: MutableStateFlow<Resource<List<Games>>> =
+        MutableStateFlow(Resource.Loading())
+    val games: StateFlow<Resource<List<Games>>>
+        get() = _games
+
+    init {
+        viewModelScope.launch {
+            _splashScreenVisible.value = true
+            _games.value = repositoryImpl.getAllGames()
+            _splashScreenVisible.value = false
+        }
+    }
+}
